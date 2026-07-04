15d257a058470ce6cf453d2a4fb4d4b484f7d88f
StarToaster <6656977+StarArawn@users.noreply.github.com>
2026-07-04 15:59:41 -0400

    DPI fixes for color picker.

--- src/widgets/color_picker/mod.rs
+++ src/widgets/color_picker/mod.rs
@@ -516,11 +516,11 @@ fn render(
 /// Renders a color picker color gradient.
 fn get_hue_gradient(color: Hsva) -> WidgetRender {
     WidgetRender::Custom {
-        render: WidgetRenderCustom::new(move |vello_scene, layout, _styles, _dpi| {
-            let location_x = layout.location.x as f64;
-            let location_y = layout.location.y as f64;
-            let size_x = layout.size.x as f64;
-            let size_y = layout.size.y as f64;
+        render: WidgetRenderCustom::new(move |vello_scene, layout, _styles, dpi| {
+            let location_x = layout.location.x as f64 * dpi as f64;
+            let location_y = layout.location.y as f64 * dpi as f64;
+            let size_x = layout.size.x as f64 * dpi as f64;
+            let size_y = layout.size.y as f64 * dpi as f64;
 
             let rect = kurbo::RoundedRect::new(
                 location_x - layout.border.left.value_or(0.0) as f64,
@@ -576,11 +576,11 @@ fn get_hue_gradient(color: Hsva) -> WidgetRender {
 /// Renders a color picker color gradient.
 fn get_saturation_gradient(color: Hsva) -> WidgetRender {
     WidgetRender::Custom {
-        render: WidgetRenderCustom::new(move |vello_scene, layout, _styles, _dpi| {
-            let location_x = layout.location.x as f64;
-            let location_y = layout.location.y as f64;
-            let size_x = layout.size.x as f64;
-            let size_y = layout.size.y as f64;
+        render: WidgetRenderCustom::new(move |vello_scene, layout, _styles, dpi| {
+            let location_x = layout.location.x as f64 * dpi as f64;
+            let location_y = layout.location.y as f64 * dpi as f64;
+            let size_x = layout.size.x as f64 * dpi as f64;
+            let size_y = layout.size.y as f64 * dpi as f64;
 
             let rect = kurbo::RoundedRect::new(
                 location_x - layout.border.left.value_or(0.0) as f64,
@@ -636,11 +636,11 @@ fn get_saturation_gradient(color: Hsva) -> WidgetRender {
 /// Renders a color picker color gradient.
 fn get_value_gradient(color: Hsva) -> WidgetRender {
     WidgetRender::Custom {
-        render: WidgetRenderCustom::new(move |vello_scene, layout, _styles, _dpi| {
-            let location_x = layout.location.x as f64;
-            let location_y = layout.location.y as f64;
-            let size_x = layout.size.x as f64;
-            let size_y = layout.size.y as f64;
+        render: WidgetRenderCustom::new(move |vello_scene, layout, _styles, dpi| {
+            let location_x = layout.location.x as f64 * dpi as f64;
+            let location_y = layout.location.y as f64 * dpi as f64;
+            let size_x = layout.size.x as f64 * dpi as f64;
+            let size_y = layout.size.y as f64 * dpi as f64;
 
             let rect = kurbo::RoundedRect::new(
                 location_x - layout.border.left.value_or(0.0) as f64,
